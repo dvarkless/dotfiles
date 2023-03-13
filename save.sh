b58afb6ea97842a94b7215587bdae30e791c2fad
dvarkless <amir911157@yandex.ru>
2023-03-14 00:56:05 +0300

    Update repositiry at 03/14/23

---
+++ save.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+cp ~/.config/nvim/lua/custom . -r
+cp ~/.zshrc .
+cp ~/.bashrc .
+cp ~/.p10k.zsh .
+
+source $JUPYTER_VENV
+pip freeze >> requirements_jupyter_venv.txt
+deactivate
+
+source $TENSORFLOW_VENV
+pip freeze >> requirements_tensorflow_venv.txt
+deactivate
+
+source ~/.venvs/nvim_venv/bin/activate
+pip freeze >> requirements_nvim_venv.txt
+deactivate
+
+pip freeze >> requirements_system.txt
+
+pamac list >> system_packages.txt
